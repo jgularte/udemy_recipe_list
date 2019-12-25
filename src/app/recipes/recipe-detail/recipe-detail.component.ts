d300dd8b59c895563d942862f51a51379ff8956f
Jack Gularte <jack.gularte@ci.security>
2019-12-25 13:32:57 -0800

    Created two services for better app structure
    Created the shopping list  and recipe services. Allows for communication between the shopping list and recipe list components

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import {ShoppingListService} from '../../shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +11,14 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  toShopping() {
+    for (let ing of this.recipe.ingredients) {
+      this.shoppingListService.addIngredient(ing);
+    }
+  }
 }
