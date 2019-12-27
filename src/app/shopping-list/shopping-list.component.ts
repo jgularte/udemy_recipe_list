122f7a2346ae21029ec593dc5a0494f2be2541f8
Jack Gularte <jack.gularte@ci.security>
2019-12-27 13:30:34 -0800

    simple changes
    added a clear function and a deleteItem button to the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {ShoppingListService} from './shopping-list.service';
+import {Ingredient} from '../common/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -17,5 +18,9 @@ export class ShoppingListComponent implements OnInit {
 
   getIngredients() {
     return this.shoppingListService.getIngredients();
+  }
+
+  deleteIngredient(ingredient: Ingredient) {
+    this.shoppingListService.deleteIngredient(ingredient);
   }
 }
