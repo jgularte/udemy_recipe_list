bc014b3da50bc3bc731b916e67b263c3a6b9292a
Jack Gularte <jack.gularte@ci.security>
2020-02-07 20:28:08 -0800

    more changes

--- src/app/modules/routing-modules/shopping-list-routing.module.ts
+++ src/app/modules/routing-modules/shopping-list-routing.module.ts
@@ -1,10 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
 import {ShoppingListComponent} from '../../shopping-list/shopping-list.component';
 import {NgModule} from '@angular/core';
+import {AuthGuard} from '../../auth/auth.guard';
 
 const routes: Routes =
   [
-    { path: '', component: ShoppingListComponent },
+    { path: '', canActivate: [AuthGuard], component: ShoppingListComponent },
   ];
 
 @NgModule({
